9cc9445ef2e0ba74a31635f92e7fb1a11f940ffb
Deividy <deividygustavo2016@gmail.com>
2026-04-08 12:36:10 -0300

    fix: case sensitive imports

--- src/Routes/privateRoute.jsx
+++ src/Routes/privateRoute.jsx
@@ -1,12 +1,12 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../context/authContext";
-// import Loading from "../../pages/Loading";
+import Loading from "../../pages/Loading";
 
 export default function PrivateRoute({ children }) {
   const { user, loading } = useContext(AuthContext);
 
-  // if (loading) return <Loading />;
+  if (loading) return <Loading />;
 
   if (!user) return <Navigate to="/" replace />;
 
